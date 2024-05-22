af1f1e4886d2bbaa242356e1595a9d41d0ede382
laertLAW <143403309+laertLAW@users.noreply.github.com>
2024-05-22 08:18:17 -0300

    pesquisa (incompleto)

--- pesquisa.php
+++ pesquisa.php
@@ -20,10 +20,24 @@
                 <h1> Pesquisar</h1>
                 <nav class="navbar bg-body-tertiary">
                     <div class="container-fluid">
-                        <form class="d-flex" role="search" action="pesquisa.php" method="POST">
-                            <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+                      <form class="d-flex" role="search" action="pesquisa.php" method="POST">
+                        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" nome="busca">
+                          <button class="btn btn-outline-sucess" type="submit">Pesquisar</button>
                         </form>
                     </div>
+                    </nav>
+                    <table class="table table-hover">
+                <thead>
+                    <tr>
+
+                       <th scope="col">Nome</th>
+                       <th scope="col">Endereço</th>
+                       <th scope="col">Telefone</th>
+                       <th scope="col">Data Nascimento</th>
+                       <th scope="col">Email</th>
+                       <th scope="col">Ações</th>
+                    </tr>
+                </thead>
             </div>
         </div>
     </div>
